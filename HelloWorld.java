7ef8d6bafc9520c2a45e56d90bee2c085699c190
Carson Allbee <callbee@dmacc.edu>
2022-01-17 23:04:10 -0600

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package week1;
+
+/**
+ * @author Carson Allbee callbee
+ *CIS175 - Fall 2021
+ * Jan 17, 2022
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+	}
+
+}
